f6f9b99bec14adda9b63c8a08e69387927d33434
Simon Törnqvist <simon.tornqvist@liu.se>
2024-05-15 18:45:29 +0200

    add colors to tailwind theme instead of pure css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,13 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      "off-white": "#fafafa",
+      black: "#000000",
+      green: "#00703a",
     },
   },
   plugins: [],
